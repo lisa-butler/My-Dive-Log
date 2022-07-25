ac467ca9ea6e914dcf80f491291f55fb490c3673
lisa-butler <l.butler1993@gmail.com>
2022-07-25 12:38:49 +0000

    trying to fix error in heroku launch

--- my_dive_log/settings.py
+++ my_dive_log/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [TEMPLATES_DIR],
-        'TEMPLATES_DIR' = 'os.path.join(BASE_DIR, 'templates')',
+        # 'TEMPLATES_DIR' = 'os.path.join(BASE_DIR, 'templates')',
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
