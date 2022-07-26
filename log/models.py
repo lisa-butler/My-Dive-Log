741bea05b91dfa18e5ffacad095dce5e5cfde7f4
lisa-butler <l.butler1993@gmail.com>
2022-07-26 11:27:26 +0000

    Error repairs and added a way to print logged dive

--- log/models.py
+++ log/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Item(models.Model):
     date = models.CharField(max_length=50, null=False, blank=False)
     location = models.CharField(max_length=50, null=False, blank=False)
@@ -8,3 +10,7 @@ class Item(models.Model):
     time = models.CharField(max_length=50, null=False, blank=False)
     buddy = models.CharField(max_length=50, null=False, blank=False)
     note = models.TextField(max_length=50, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.date
--- log/views.py
+++ log/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
+
+
 def get_logpage(request):
-    return render(request, "log/logpage.html")
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, "log/logpage.html", context)
+
