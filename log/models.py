550b581546a56bb318f8d345cdf784eaa2e176a9
lisa-butler <l.butler1993@gmail.com>
2022-07-26 10:36:38 +0000

    dive log items class created and items added

--- log/models.py
+++ log/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    date = models.CharField(max_length=50, null=False, blank=False)
+    location = models.CharField(max_length=50, null=False, blank=False)
+    depth = models.CharField(max_length=50, null=False, blank=False)
+    time = models.CharField(max_length=50, null=False, blank=False)
+    buddy = models.CharField(max_length=50, null=False, blank=False)
+    note = models.TextField(max_length=50, null=True, blank=True)
