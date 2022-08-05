7c8209c80fe571cc4892747d661084c1824350bb
lisa-butler <l.butler1993@gmail.com>
2022-08-05 13:32:01 +0000

    prepared to deploy to heroku

--- dive_log/settings.py
+++ dive_log/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +76,16 @@ WSGI_APPLICATION = 'dive_log.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://wlxqgtgjqwnklt:7f96ec867f9dc0d4bf18a3034ec296b7543a1d773290912a78ed2f26f39cf7d6@ec2-34-253-119-24.eu-west-1.compute.amazonaws.com:5432/d6ga14me8q11bu')
+
 }
 
 
