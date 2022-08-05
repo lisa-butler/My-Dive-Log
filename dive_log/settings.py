d5fa934755b27a2be8988f6ae7699e27eccb2f63
lisa-butler <l.butler1993@gmail.com>
2022-08-05 14:53:11 +0000

    troubleshooting, commented out line in datatbases

--- dive_log/settings.py
+++ dive_log/settings.py
@@ -73,8 +73,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'dive_log.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/3.2/ref/settings/databases
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/databases
 
 
 # DATABASES = {
