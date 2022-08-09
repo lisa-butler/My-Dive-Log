b8c8041808f045862691250466c92a3299d0f030
lisa-butler <l.butler1993@gmail.com>
2022-08-09 19:17:08 +0000

    uninstalled material ui and installed bootstrap

--- log/forms.py
+++ log/forms.py
@@ -6,3 +6,7 @@ class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ['date', 'location', 'depth', 'time', 'buddy', 'note']
+        widgets = {
+            'date' : forms.TextInput(attrs={'class': 'form-control'}),
+            'location' : forms.TextInput(attrs={'class': 'form-control'}),
+        }
