529231abc5e6dd5149e837cc12951019ee267a3c
lisa-butler <l.butler1993@gmail.com>
2022-08-18 16:04:01 +0000

    added a base.html and edited templates for signin

--- dive_log/settings.py
+++ dive_log/settings.py
@@ -20,6 +20,7 @@ development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
 
 # Quick-start development settings - unsuitable for production
