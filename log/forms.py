4923b6a7762a19e6a0be1fca609a10990f207bdb
lisa-butler <l.butler1993@gmail.com>
2022-09-01 20:05:16 +0000

    Register function working

--- log/forms.py
+++ log/forms.py
@@ -24,3 +24,5 @@ class RegisterUserForm(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
     first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
     last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+
+    
