7415d2ba280b2c835088e9424fae3df82bb442b4
lisa-butler <l.butler1993@gmail.com>
2022-09-27 10:16:04 +0000

    style changes made to colors, added cards

--- log/forms.py
+++ log/forms.py
@@ -21,14 +21,6 @@ class ItemForm(forms.ModelForm):
         }
 
 
-# class RegisterUserForm(UserCreationForm):
-#     model = Info
-#     username = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
-#     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
-#     first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
-#     last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
-
-
 
 
 class NewUserForm(UserCreationForm):
